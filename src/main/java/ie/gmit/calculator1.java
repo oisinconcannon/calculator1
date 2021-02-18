4e26640883980705a11b118b8f52551d823f7aca
Oisin <G00347603@gmit.ie>
2021-02-18 14:46:10 +0000

    Update lab work

--- src/main/java/ie/gmit/calculator1.java
+++ src/main/java/ie/gmit/calculator1.java
@@ -1,8 +1,30 @@
-//Oisin
+/*
+Oisin Concannon
+Software Eng w/Test
+G00347603
+18/02/21
+ */
 package ie.gmit;
 
 public class calculator1 {
-    static void add(int firstNum, int secondNum) {
+    public int add(int firstNum, int secondNum)
+    {
         int result = firstNum + secondNum;
+        return result;
+    }
+    public int subtract(int firstNum, int secondNum)
+    {
+        int result = firstNum - secondNum;
+        return result;
+    }
+    public int divide(int firstNum, int secondNum)
+    {
+        int result = firstNum / secondNum;
+        return result;
+    }
+    public int multiply(int firstNum, int secondNum)
+    {
+        int result = firstNum * secondNum;
+        return result;
     }
 }
--- src/test/java/ie/gmit/calculator1Test.java
+++ src/test/java/ie/gmit/calculator1Test.java
@@ -1,4 +1,44 @@
+/*
+Oisin Concannon
+Software Eng w/Test
+G00347603
+18/02/21
+ */
+
 package ie.gmit;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class calculator1Test {
+
+    @Test
+    void addTest(){
+       calculator1 myCalc = new calculator1();
+       int result = myCalc.add(10,50);
+       assertEquals(60,result,0);
+
+    }
+
+    @Test
+    void subtractTest(){
+        calculator1 myCalc = new calculator1();
+        int result = myCalc.subtract(12,6);
+        assertEquals(6,result,0);
+
+    }
+    @Test
+    void divideTest(){
+        calculator1 myCalc = new calculator1();
+        int result = myCalc.divide(20,5);
+        assertEquals(4,result,0);
+
+    }
+    @Test
+    void multiplyTest(){
+        calculator1 myCalc = new calculator1();
+        int result = myCalc.multiply(6,3);
+        assertEquals(18,result,0);
+
+    }
 }
